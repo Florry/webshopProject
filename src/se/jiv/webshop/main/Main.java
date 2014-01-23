a5f1686cf9738601c4e395edbb62e9577ec0aa06
Isabella Rodriguez <isabella.rodriguez@live.se>
2014-01-23 16:17:40 +0100

    added main.java

--- src/se/jiv/webshop/main/Main.java
+++ src/se/jiv/webshop/main/Main.java
@@ -1,16 +1,16 @@
 package se.jiv.webshop.main;
 
 import se.jiv.webshop.model.*;
+import se.jiv.webshop.repository.dao.*;
 
 public class Main
 {
+
 	public static void main(String args[])
 	{
-		// TEST FILES
-		ProductModel product1 = new ProductModel("apple", 20, "a fruit", 300);
-		UserModel user1 = new UserModel("apple@apple.com", "Password001", "Bob", "Johnson",
-				"1940-03-04", "0704438573", "Roadrd. 2", "C/O bobson", "New York", "19587");
+		ProductModel newProduct = new ProductModel("Mylo Xyloto", 149);
+		ProductDAO productHandler = new ProductDAO();
+		System.out.println(productHandler.updateProduct("VIVA LA VIDA", productHandler.PRODUCT_COST, "200"));
 		
-		System.out.println(String.format("%s %n%n%s", product1.toString(), user1.toString()));
 	}
 }
