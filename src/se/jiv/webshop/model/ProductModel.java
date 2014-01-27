e3c21d61b09379b2c6a4a5df72deedbacf551088
Sunsette <isabella.rodriguez@live.se>
2014-01-27 09:16:43 +0100

    Update ProductModel.java
    updated ProductModel.java

--- src/se/jiv/webshop/model/ProductModel.java
+++ src/se/jiv/webshop/model/ProductModel.java
@@ -6,6 +6,7 @@ public final class ProductModel
 	String description;
 	double cost;
 	double rrp;
+	int id;
 
 	public ProductModel(String name, double cost)
 	{
@@ -30,6 +31,27 @@ public final class ProductModel
 		this(name, cost, description);
 		this.rrp = rrp;
 	}
+	public ProductModel(int id,String name, double cost, String description, double rrp)
+	{
+		this(name, cost, description, rrp);
+		this.id = id;
+	}
+
+	public ProductModel(ProductModel other)
+	{
+		this.name = other.name;
+		this.description = other.description;
+		this.cost = other.cost;
+		this.rrp = other.rrp;
+	}
+	public ProductModel(int id, ProductModel other)
+	{
+		this.id = id;
+		this.name = other.name;
+		this.description = other.description;
+		this.cost = other.cost;
+		this.rrp = other.rrp;
+	}
 
 	public String getName()
 	{
@@ -50,10 +72,11 @@ public final class ProductModel
 	{
 		return this.rrp;
 	}
-	
+
 	@Override
-	public String toString(){
-		return String.format("Name: " + getName() + "%nCost: " + getCost() + 
+	public String toString()
+	{
+		return String.format("Id: " + id + "%nName: " + getName() + "%nCost: " + getCost() +
 				"%nDescription: " + getDescription() + "%nRRP: " + getRrp());
 	}
 
