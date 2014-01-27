f223c62636dfc054a803ed9cca2b66e04e8ecbd8
florry <soderstrom.viktor@gmail.com>
2014-01-27 14:32:01 +0100

    main

--- src/se/jiv/webshop/main/Main.java
+++ src/se/jiv/webshop/main/Main.java
@@ -3,24 +3,25 @@ package se.jiv.webshop.main;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import se.jiv.webshop.exception.WebshopAppException;
 import se.jiv.webshop.model.UserModel;
 import se.jiv.webshop.repository.dao.UserDAO;
 import se.jiv.webshop.service.UserService;
 
 public class Main
 {
-	public static void main(String args[])
+	public static void main(String args[]) throws WebshopAppException
 	{
 		// TEST FILES
 		UserService userService = new UserService(new UserDAO());
 		
 		UserModel user1 = userService.getUser("sed2.facilisis@Proinnon.edu");
 		Map<Integer, Integer> contents = new LinkedHashMap<Integer, Integer>();
-		userService.addProductToCart(user1, 1);
-		userService.addProductToCart(user1, 1);
-		userService.addProductToCart(user1, 1);
-		userService.addProductToCart(user1, 2);
-		userService.addProductToCart(user1, 2);
-		userService.addProductToCart(user1, 2);
+		userService.addProductToCart(user1, 1, 20);
+		userService.addProductToCart(user1, 1, 10);
+		userService.addProductToCart(user1, 1, 1);
+		userService.addProductToCart(user1, 2, 1);
+		userService.addProductToCart(user1, 2, 2);
+		userService.addProductToCart(user1, 2, 30);
 	}
 }
