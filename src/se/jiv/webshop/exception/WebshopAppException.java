c2d3579b6d151c130cbaf15dfa979fb1961f3c45
JorgeLegarre <enkidugan@gmail.com>
2014-01-28 09:20:37 +0100

    JLP --> Exception Updaterad

--- src/se/jiv/webshop/exception/WebshopAppException.java
+++ src/se/jiv/webshop/exception/WebshopAppException.java
@@ -1,18 +1,26 @@
 package se.jiv.webshop.exception;
 
+import java.sql.SQLException;
+
 public final class WebshopAppException extends Exception {
 
 	private static final long serialVersionUID = 20140123L;
-
+	
+	private String className;
 	private String actionName;
 
-	public WebshopAppException(String message, String actionName) {
-		super(message);
+	public WebshopAppException(SQLException e, String className, String actionName) {
+		super(e);
+		this.className = className;
 		this.actionName = actionName;
 	}
 
 	public String getActionName() {
 		return actionName;
+	}
+	
+	public String getClassName() {
+		return className;
 	}
 
 }
