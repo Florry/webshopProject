32a647e71d0b6e561d6c166c0b5279feddcacf79
Isabella Rodriguez <isabella.rodriguez@live.se>
2014-01-29 10:58:49 +0100

    changes

--- src/se/jiv/webshop/ui/MainMenuUI.java
+++ src/se/jiv/webshop/ui/MainMenuUI.java
@@ -17,20 +17,6 @@ public class MainMenuUI extends GeneralUI
 
 	}
 
-	public String logInEmail()
-	{
-		System.out.println("Please enter your email: ");
-
-		return readString();
-	}
-
-	public String logInPassword()
-	{
-		System.out.println("Please enter your password: ");
-
-		return readString();
-	}
-
 	public int retrieveInformation()
 	{
 		System.out.println("About what do you want to retrieve information?");
@@ -41,46 +27,6 @@ public class MainMenuUI extends GeneralUI
 		return readInt();
 	}
 
-	public String productInformation()
-	{
-		System.out.println("Please enter the name of the product you want to "
-				+ "get information about");
 
-		return readString();
-	}
-
-	public String categoryInformation()
-	{
-		System.out.println("Please enter the name of the category you want to "
-				+ "get information about: ");
-
-		return readString();
-	}
-
-	public String userInformation()
-	{
-		System.out.println("Please enter the name of the user you want to "
-				+ "get information about: ");
-
-		return readString();
-	}
-
-	public int productsByCategory()
-	{
-		System.out.println("What category do you want to search for?");
-		System.out.println("1. Books");
-		System.out.println("2. Children");
-		System.out.println("3. Clothes");
-		System.out.println("4. Computing");
-		System.out.println("5. Electronics");
-		System.out.println("6. Films");
-		System.out.println("7. Gardening");
-		System.out.println("8. Mens");
-		System.out.println("9. Sports and outdoors");
-		System.out.println("10. Toys");
-		System.out.println("11. Womens");
-
-		return readInt();
-	}
 
 }
