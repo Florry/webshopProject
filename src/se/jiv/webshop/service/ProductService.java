6a53e429d7430e4f56f98818a904b7d176dbd2c0
Isabella Rodriguez <isabella.rodriguez@live.se>
2014-01-29 12:04:32 +0100

    changes

--- src/se/jiv/webshop/service/ProductService.java
+++ src/se/jiv/webshop/service/ProductService.java
@@ -8,44 +8,50 @@ import se.jiv.webshop.repository.ProductRepository;
 public class ProductService
 {
 	private ProductRepository productRepository;
-	
+
 	public ProductService(ProductRepository productRepository)
 	{
 		this.productRepository = productRepository;
 	}
-	
+
 	public ProductModel createProduct(ProductModel product)
 	{
 		return productRepository.createProduct(product);
 	}
-	
+
 	public boolean updateProduct(ProductModel product, int productId)
 	{
 		return productRepository.updateProduct(product, productId);
 	}
-	
+
 	public boolean deleteProduct(int productId)
 	{
 		return productRepository.deleteProduct(productId);
 	}
-	
+
 	public List<ProductModel> getProductByName(String name)
 	{
 		return productRepository.getProductByName(name);
 	}
-	
+
 	public List<ProductModel> getProductsByCost(double cost)
 	{
 		return productRepository.getProductsByCost(cost);
 	}
-	
+
 	public ProductModel getProductById(int id)
 	{
 		return productRepository.getProductById(id);
 	}
-	
+
 	public List<ProductModel> getAllProducts()
 	{
 		return productRepository.getAllProducts();
 	}
+
+	public List<ProductModel> getProductsByCategory(int categoryId)
+	{
+		return productRepository.getProductsByCategory(categoryId);
+	}
+
 }
