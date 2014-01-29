2ec4366ca07c329a0e2807d783060012b94f5ece
Isabella Rodriguez <isabella.rodriguez@live.se>
2014-01-29 10:26:00 +0100

    changed MainMenu

--- src/se/jiv/webshop/ui/MainMenuUI.java
+++ src/se/jiv/webshop/ui/MainMenuUI.java
@@ -3,6 +3,18 @@ package se.jiv.webshop.ui;
 public class MainMenuUI extends GeneralUI
 {
 
-
-
+	public int firstMenu()
+	{
+		System.out.println("Welcome Admin");
+		System.out.println("These are your options:");
+		System.out.println("1. Log in");
+		System.out.println("2. Retrieve information");
+		System.out.println("3. Retrieve shopping cart");
+		System.out.println("4. Search for product by category");
+		
+		return readInt();
+		
+	}
+	
+	
 }
