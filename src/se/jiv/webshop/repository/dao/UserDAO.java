7cce4f6d4129f00a374b3985ca1be772ea9718b2
florry <soderstrom.viktor@gmail.com>
2014-02-11 09:11:19 +0100

    fadsfa

--- src/se/jiv/webshop/repository/dao/UserDAO.java
+++ src/se/jiv/webshop/repository/dao/UserDAO.java
@@ -7,155 +7,236 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import se.jiv.webshop.exception.WebshopAppException;
 import se.jiv.webshop.model.UserModel;
 import se.jiv.webshop.repository.UserRepository;
+import se.jiv.webshop.service.UserService;
 
-public final class UserDAO extends GeneralDAO implements UserRepository {
-
+public final class UserDAO extends GeneralDAO implements UserRepository
+{
+	private static final Logger LOGGER = Logger.getLogger(UserDAO.class.getSimpleName());
+	
+	public static void main(String args[]) throws WebshopAppException
+	{
+		UserService us = new UserService(new UserDAO(), new ShoppingCartDAO());
+		us.deleteUser(new UserModel.Builder("ok@ok.se", "123", "Test", "Testson",
+				"Stockholmsv. 32", "Stockholm", "postcode").build());
+	}
+	
 	@Override
-	public void addUser(UserModel user) throws WebshopAppException {
-		if (isValidUser(user, "ADD_USER")) {
-
-			try (Connection conn = getConnection()) {
-
+	public void addUser(UserModel user) throws WebshopAppException
+	{
+		if (isValidUser(user, "ADD_USER"))
+		{
+			
+			try (Connection conn = getConnection())
+			{
+				
 				String sql = "INSERT INTO users (password, firstname, lastname, dob, telephone, address1, address2, town, postcode, email)"
 						+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-				try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+				try (PreparedStatement pstmt = conn.prepareStatement(sql))
+				{
 					prepareStatementFromModel(pstmt, user);
-
+					
 					pstmt.executeUpdate();
-
+					LOGGER.trace("User added: " + user);
 				}
-
-			} catch (SQLException e) {
-				throw new WebshopAppException(e.getMessage(), this.getClass()
+				
+			} catch (SQLException e)
+			{
+				WebshopAppException excep = new WebshopAppException(e.getMessage(), this.getClass()
 						.getSimpleName(), "ADD_USER");
+				LOGGER.error(excep);
+				throw excep;
 			}
 		}
-
+		
 	}
-
+	
 	@Override
-	public void updateUser(UserModel user) throws WebshopAppException {
-
-		if (isValidUser(user, "UPDATE_USER")) {
-
-			try (Connection conn = getConnection()) {
-
+	public void updateUser(UserModel user) throws WebshopAppException
+	{
+		
+		if (isValidUser(user, "UPDATE_USER"))
+		{
+			
+			try (Connection conn = getConnection())
+			{
+				
 				String sql = "UPDATE users SET password = ?, firstname = ?, lastname = ?, dob = ?, telephone = ?, address1 = ?, "
 						+ "address2 = ?, town = ?, postcode = ? WHERE email = ?";
-
-				try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+				
+				try (PreparedStatement pstmt = conn.prepareStatement(sql))
+				{
 					prepareStatementFromModel(pstmt, user);
-
+					
 					pstmt.executeUpdate();
+					LOGGER.trace("User updated: " + user);
 				}
-			} catch (SQLException e) {
-				throw new WebshopAppException(e.getMessage(), this.getClass()
+			} catch (SQLException e)
+			{
+				WebshopAppException excep = new WebshopAppException(e.getMessage(), this.getClass()
 						.getSimpleName(), "UPDATE_USER");
+				LOGGER.error(excep);
+				throw excep;
 			}
+		} else
+		{
+			LOGGER.error("Update user: User is null.");
 		}
-
+		
 	}
-
+	
 	@Override
-	public void deleteUser(UserModel user) throws WebshopAppException {
-
-		if (isValidUser(user, "DELETE_USER")) {
-
-			try (Connection conn = getConnection()) {
-
+	public void deleteUser(UserModel user) throws WebshopAppException
+	{
+		
+		if (isValidUser(user, "DELETE_USER"))
+		{
+			
+			try (Connection conn = getConnection())
+			{
+				
 				String sql = "DELETE FROM users WHERE email = ?";
-
-				try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+				
+				try (PreparedStatement pstmt = conn.prepareStatement(sql))
+				{
 					setString(pstmt, 1, user.getEmail());
-
+					
 					pstmt.executeUpdate();
+					LOGGER.trace("User deleted: " + user);
 				}
-
-			} catch (SQLException e) {
-				throw new WebshopAppException(e.getMessage(), this.getClass()
+				
+			} catch (SQLException e)
+			{
+				WebshopAppException excep = new WebshopAppException(e.getMessage(), this.getClass()
 						.getSimpleName(), "DELETE_USER");
+				LOGGER.error(excep);
+				throw excep;
 			}
+		} else
+		{
+			LOGGER.error("Delete user: User is null.");
 		}
 	}
-
+	
 	@Override
-	public UserModel getUser(String email) throws WebshopAppException {
-		if (isValidEmail(email, "GET_USER")) {
-
-			try (Connection conn = getConnection()) {
-
+	public UserModel getUser(String email) throws WebshopAppException
+	{
+		if (isValidEmail(email, "GET_USER"))
+		{
+			
+			try (Connection conn = getConnection())
+			{
+				
 				String sql = "SELECT * FROM users WHERE email = ?";
-
-				try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+				
+				try (PreparedStatement pstmt = conn.prepareStatement(sql))
+				{
 					setString(pstmt, 1, email);
-
-					try (ResultSet rs = pstmt.executeQuery()) {
-
-						if (rs.next()) {
+					
+					try (ResultSet rs = pstmt.executeQuery())
+					{
+						
+						if (rs.next())
+						{
+							LOGGER.trace("User get from database: " + parseModel(rs));
 							return parseModel(rs);
 						}
 					}
+					
 				}
-
-			} catch (SQLException e) {
-				throw new WebshopAppException(e.getMessage(), this.getClass()
+				
+			} catch (SQLException e)
+			{
+				WebshopAppException excep = new WebshopAppException(e.getMessage(), this.getClass()
 						.getSimpleName(), "GET_USER");
+				LOGGER.error(excep);
+				throw excep;
 			}
+		} else
+		{
+			LOGGER.error("Get user: Email is null.");
 		}
+		
 		return null;
 	}
-
+	
 	@Override
-	public List<UserModel> getAllUsers() throws WebshopAppException {
-
-		try (Connection conn = getConnection()) {
-
+	public List<UserModel> getAllUsers() throws WebshopAppException
+	{
+		
+		try (Connection conn = getConnection())
+		{
+			
 			String sql = "SELECT * FROM users";
-			try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-
-				try (ResultSet rs = pstmt.executeQuery()) {
-
+			try (PreparedStatement pstmt = conn.prepareStatement(sql))
+			{
+				
+				try (ResultSet rs = pstmt.executeQuery())
+				{
+					
 					List<UserModel> userList = new ArrayList<UserModel>();
-					while (rs.next()) {
+					while (rs.next())
+					{
 						userList.add(parseModel(rs));
-
+						
 					}
+					LOGGER.trace("All users get from database");
 					return userList;
 				}
 			}
-		} catch (SQLException e) {
-			throw new WebshopAppException(e.getMessage(), this.getClass()
+		} catch (SQLException e)
+		{
+			WebshopAppException excep = new WebshopAppException(e.getMessage(), this.getClass()
 					.getSimpleName(), "GET_ALL_USERS");
+			LOGGER.error(excep);
+			throw excep;
 		}
-
+		
 	}
-
+	
 	@Override
-	public boolean validateLogin(String email, String password)
-			throws WebshopAppException {
-		try (Connection conn = getConnection()) {
-
+	public boolean validateLogin(String email, String password) throws WebshopAppException
+	{
+		try (Connection conn = getConnection())
+		{
+			
 			String sql = "SELECT * FROM users WHERE email = ? and password = ?";
-
-			try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			
+			try (PreparedStatement pstmt = conn.prepareStatement(sql))
+			{
 				setString(pstmt, 1, email);
 				setString(pstmt, 2, password);
-
-				try (ResultSet rs = pstmt.executeQuery()) {
-					return rs.next();
+				
+				try (ResultSet rs = pstmt.executeQuery())
+				{
+					if (rs.next())
+					{
+						LOGGER.trace("User validated: " + email);
+						return true;
+					} else
+					{
+						LOGGER.trace("User not valid: " + email);
+						return false;
+					}
 				}
 			}
-
-		} catch (SQLException e) {
-			throw new WebshopAppException(e.getMessage(), this.getClass()
+			
+		} catch (SQLException e)
+		{
+			WebshopAppException excep = new WebshopAppException(e.getMessage(), this.getClass()
 					.getSimpleName(), "VALIDATE_LOGIN");
+			LOGGER.error(excep);
+			throw excep;
 		}
+		
 	}
-
-	private UserModel parseModel(ResultSet rs) throws SQLException {
+	
+	private UserModel parseModel(ResultSet rs) throws SQLException
+	{
 		String db_email = rs.getString("email");
 		String db_password = rs.getString("password");
 		String db_firstname = rs.getString("firstname");
@@ -166,15 +247,15 @@ public final class UserDAO extends GeneralDAO implements UserRepository {
 		String db_address2 = rs.getString("address2");
 		String db_town = rs.getString("town");
 		String db_postcode = rs.getString("postcode");
-
-		return new UserModel.Builder(db_email, db_password, db_firstname,
-				db_lastname, db_address1, db_town, db_postcode)
-				.address2(db_address2).dob(db_dob).telephone(db_telephone)
+		
+		return new UserModel.Builder(db_email, db_password, db_firstname, db_lastname, db_address1,
+				db_town, db_postcode).address2(db_address2).dob(db_dob).telephone(db_telephone)
 				.build();
 	}
-
-	private void prepareStatementFromModel(PreparedStatement pstmt,
-			UserModel user) throws SQLException {
+	
+	private void prepareStatementFromModel(PreparedStatement pstmt, UserModel user)
+			throws SQLException
+	{
 		pstmt.setString(1, user.getPassword());
 		pstmt.setString(2, user.getFirstname());
 		pstmt.setString(3, user.getLastname());
@@ -185,26 +266,32 @@ public final class UserDAO extends GeneralDAO implements UserRepository {
 		pstmt.setString(8, user.getTown());
 		pstmt.setString(9, user.getPostcode());
 		pstmt.setString(10, user.getEmail());
-
+		
 	}
-
-	private boolean isValidUser(UserModel user, String functionName)
-			throws WebshopAppException {
-		if (user == null) {
-			throw new WebshopAppException("user can not be null", this
+	
+	private boolean isValidUser(UserModel user, String functionName) throws WebshopAppException
+	{
+		if (user == null)
+		{
+			WebshopAppException excep = new WebshopAppException("user can not be null", this
 					.getClass().getSimpleName(), functionName);
+			LOGGER.error(excep);
+			throw excep;
 		}
-
+		
 		return true;
 	}
-
-	private boolean isValidEmail(String email, String functionName)
-			throws WebshopAppException {
-		if (email == null) {
-			throw new WebshopAppException("Email can't be null", this
+	
+	private boolean isValidEmail(String email, String functionName) throws WebshopAppException
+	{
+		if (email == null)
+		{
+			WebshopAppException excep = new WebshopAppException("Email can't be null", this
 					.getClass().getSimpleName(), functionName);
+			LOGGER.error(excep);
+			throw excep;
 		}
 		return true;
 	}
-
+	
 }
