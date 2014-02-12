808a7c6418b2f355e2bd6348e9e2e6371f92a034
florry <soderstrom.viktor@gmail.com>
2014-02-12 11:45:37 +0100

    Author model updated, author test updated, author repository added, author dao added, author service added.

--- src/se/jiv/webshop/test/AuthorModelTest.java
+++ src/se/jiv/webshop/test/AuthorModelTest.java
@@ -1,11 +1,15 @@
 package se.jiv.webshop.test;
 
+import se.jiv.webshop.exception.WebshopAppException;
 import se.jiv.webshop.model.AuthorModel;
+import se.jiv.webshop.repository.dao.AuthorDAO;
+import se.jiv.webshop.service.AuthorService;
 
 public class AuthorModelTest
 {
 	public static void main(String args[])
 	{
+		AuthorService authors = new AuthorService(new AuthorDAO());
 		AuthorModel author = new AuthorModel.Builder("Harry", "Otter").dob("1962-03-19")
 				.country("Finland").build();
 		AuthorModel author2 = new AuthorModel.Builder("Perry", "Otter").dob("1962-03-19")
@@ -30,5 +34,44 @@ public class AuthorModelTest
 				author3.getLastname(), author3.hashCode()));
 		System.out.println(String.format("author4: %s %s's hashcode = %s", author4.getFirstname(),
 				author4.getLastname(), author4.hashCode()));
+		
+		System.out.println("Author: " + author + " added");
+		
+		try
+		{
+			authors.addAuthor(author);
+		} catch (WebshopAppException e)
+		{
+			e.printStackTrace();
+		}
+		try
+		{
+			authors.updateAuthor(author2, 7);
+			authors.deleteAuthor(7);
+		} catch (WebshopAppException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try
+		{
+			System.out.println(authors.getAuthor(12));
+		} catch (WebshopAppException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		try
+		{
+			System.out.println(authors.getAuthorsByName("harry"));
+			System.out.println(authors.getAllAuthors());
+		} catch (WebshopAppException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
